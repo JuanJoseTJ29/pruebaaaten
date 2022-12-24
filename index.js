fdf4d8478e31b0f75c6411b4ee6056804f474e80
JuanJoseTJ29 <juan.tirado@unmsm.edu.pe>
2022-12-24 03:02:35 -0500

    Revert "Revert " prueba66""
    This reverts commit 41737d456ca73f2b32c317d746b15da8e481862b.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 /* HOLAAAAAAAAAAAAAAAAAAAAAAAA */
-/* adiossssssssssssssssssssssss */
+/* ALOJAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA*/
 /* eslint-disable no-console */
 import 'dotenv/config'
 import express from 'express'
